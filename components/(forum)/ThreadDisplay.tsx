97996f2d8cfb72702169f372c53e0a50b1eb108f
MartinPettersen <searchingtheskyfor@gmail.com>
2024-06-11 11:45:00 +0200

    fix layout bug for forum thread

--- components/(forum)/ThreadDisplay.tsx
+++ components/(forum)/ThreadDisplay.tsx
@@ -3,59 +3,49 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 type Props = {
-    subject: string,
-    author: string,
-    date: string,
-    replies: string,
-    id: string,
-}
+  subject: string;
+  author: string;
+  date: string;
+  replies: string;
+  id: string;
+};
 
-const ThreadDisplay = ({subject, author, date, replies, id}: Props) => {
-
-
+const ThreadDisplay = ({ subject, author, date, replies, id }: Props) => {
   const navigation = useNavigation();
 
-
   return (
-
-
     <TouchableOpacity
-    style={styles.display}
-    onPress={() =>
-      navigation.navigate("ThreadPage", { id } )
-    }
-  >
-
-    <View  style={styles.display}>
-      <Text style={styles.displayText}>{subject}</Text>
-      <Text style={styles.displayText}>{author}</Text>
-      <Text style={styles.displayText}>{date}</Text>
-      <Text style={styles.displayText}>{`#${replies}`}</Text>
-    </View>
-  </TouchableOpacity>
-
+      
+      onPress={() => navigation.navigate("ThreadPage", { id })}
+    >
+      <View style={styles.display}>
+        <Text style={styles.displayText}>{subject}</Text>
+        <Text style={styles.displayText}>{author}</Text>
+        <Text style={styles.displayText}>{date}</Text>
+        <Text style={styles.displayText}>{`#${replies}`}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-    
-    display: {
-      backgroundColor: "#27272a",
-      width: "90%",
-      justifyContent: "space-between",
-      alignItems: "center",
-      height: 50,
-      marginBottom: 20,
-      flexDirection: 'row'
-    },
-    displayText: {
-      fontSize: 12,
-      fontWeight: 'bold',
-      marginVertical: 10,
-      marginHorizontal: 6,
+  display: {
+    backgroundColor: "#27272a",
+    width: "90%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 50,
+    marginBottom: 20,
+    flexDirection: "row",
+  },
+  displayText: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginVertical: 10,
+    marginHorizontal: 6,
 
-      color: "white",
-    },
-  });
+    color: "white",
+  },
+});
 
 export default ThreadDisplay;
